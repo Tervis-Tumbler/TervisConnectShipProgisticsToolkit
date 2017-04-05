23b8f35b781356cd4a42e05b04b340c96ca9cb3f
Chris Magnuson <hourglasssand@gmail.com>
2017-04-05 15:01:12 -0400

    Remove old non relevant stuff

--- TervisConnectShipProgisticsToolkit.psm1
+++ TervisConnectShipProgisticsToolkit.psm1
@@ -4,10 +4,4 @@
     )
     Invoke-ClusterApplicationProvision -ClusterApplicationName Progistics -EnvironmentName $EnvironmentName
     $Nodes = Get-TervisClusterApplicationNode -ClusterApplicationName Progistics -EnvironmentName $EnvironmentName
-    $Nodes | Install-WCSPrintersForBartenderCommander
-    $Nodes | Add-WCSODBCDSN -ODBCDSNTemplateName Tervis
-    $Nodes | Add-WCSODBCDSN -ODBCDSNTemplateName tervisBartender
-    $Nodes | Set-TervisBTLMIniFile
-    $Nodes | Set-TervisCommanderTaskList
-    $Nodes | New-BartenderCommanderFirewallRules
 }
