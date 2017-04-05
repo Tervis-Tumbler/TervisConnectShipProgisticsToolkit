e1b8058201700cc7004cf3ac12db0e15afa895e5
Chris Magnuson <hourglasssand@gmail.com>
2017-04-05 12:31:07 -0400

    Initial commit

---
+++ TervisConnectShipProgisticsToolkit.psm1
@@ -0,0 +1,13 @@
+﻿function Invoke-ProgisticsProvision {
+    param (
+        $EnvironmentName
+    )
+    Invoke-ClusterApplicationProvision -ClusterApplicationName Progistics -EnvironmentName $EnvironmentName
+    $Nodes = Get-TervisClusterApplicationNode -ClusterApplicationName Progistics -EnvironmentName $EnvironmentName
+    $Nodes | Install-WCSPrintersForBartenderCommander
+    $Nodes | Add-WCSODBCDSN -ODBCDSNTemplateName Tervis
+    $Nodes | Add-WCSODBCDSN -ODBCDSNTemplateName tervisBartender
+    $Nodes | Set-TervisBTLMIniFile
+    $Nodes | Set-TervisCommanderTaskList
+    $Nodes | New-BartenderCommanderFirewallRules
+}
